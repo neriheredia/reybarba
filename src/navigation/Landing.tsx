e6b646d51ebcab8de41f7a8742aace4ed4c8fba0
neriheredia <developer.heredia@gmail.com>
2022-07-16 17:23:10 -0300

    fix: navigation

--- src/navigation/Landing.tsx
+++ src/navigation/Landing.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import NavigationTab from "./NavigationTab";
-import LandingPage from '../screens/Landing/Landing';
+import { LandingPage } from "../screens/LandingPage";
 
 const Stack = createNativeStackNavigator();
 
 const Landing = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="LandingPage" component={LandingPage}
+      <Stack.Screen
+        name="LandingPage"
+        component={LandingPage}
         options={{
-          headerShown:false
+          headerShown: false,
         }}
       />
       <Stack.Screen name="NavigationTab" component={NavigationTab} />
--- src/navigation/NavigationTab.tsx
+++ src/navigation/NavigationTab.tsx
@@ -7,31 +7,30 @@ import { colors } from "../constants/colors";
 import DiscountsStack from "./DiscountsStack";
 import AcountStack from "./AcountStack";
 import TurnsStack from "../screens/TurnsStack/TurnsStack";
-import Header from '../shared/components/Header/Header';
-import { TAB_BAR_HEIGHT } from '../helpers/app';
-import { colors } from '../constants/colors';
+import Header from "../shared/components/Header/Header";
+import { TAB_BAR_HEIGHT } from "../helpers/app";
 
 const Tab = createBottomTabNavigator();
 
 const NavigationTab = () => {
   return (
     <Tab.Navigator
-    screenOptions={({ route }) => ({
-      headerShown: false, 
-      tabBarShowLabel: false,
-      tabBarStyle: {
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
           backgroundColor: colors.brown,
           height: TAB_BAR_HEIGHT,
-          borderColor:colors.brown
-      },
-      tabBarIcon: ({ color }) => screenOptions(route, color),
-      tabBarHideOnKeyboard: true,
-      tabBarInactiveTintColor: colors.gray,
-      tabBarActiveTintColor: colors.white,
-      headerStyle:{
-        backgroundColor:colors.orange,
-      },
-    })} 
+          borderColor: colors.brown,
+        },
+        tabBarIcon: ({ color }) => screenOptions(route, color),
+        tabBarHideOnKeyboard: true,
+        tabBarInactiveTintColor: colors.gray,
+        tabBarActiveTintColor: colors.white,
+        headerStyle: {
+          backgroundColor: colors.orange,
+        },
+      })}
     >
       <Tab.Screen
         name="Home"
@@ -40,13 +39,12 @@ const NavigationTab = () => {
       />
       <Tab.Screen name="Descuentos" component={DiscountsStack} />
       <Tab.Screen
-      
         name="Turnos"
         component={TurnsStack}
         options={{
-          headerShown:false,
-          headerTintColor:"red",
-      }}
+          headerShown: false,
+          headerTintColor: "red",
+        }}
       />
       <Tab.Screen name="Cuenta" component={AcountStack} />
     </Tab.Navigator>
